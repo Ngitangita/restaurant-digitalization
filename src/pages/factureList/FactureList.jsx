4c38e7ca45983805464097c38a9cc27d4cba2d86
Hevitriniavo <hei.tantely@gmail.com>
2025-01-12 14:22:54 +0300

    feat: ordersByTables

--- src/pages/factureList/FactureList.jsx
+++ src/pages/factureList/FactureList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { apiUrl } from '../../services/api';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
